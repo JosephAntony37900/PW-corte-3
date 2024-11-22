bd7904f456fb52d86f51d5f811f5b34117de4d5b
JosephAntony37900 <Joseantoniopintoaguilar@gmail.com>
2024-11-22 14:12:14 -0600

    characters and locations with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LocationsComponent } from './locations/locations.component';
+import { CardsComponent } from './cards/cards.component';
 
 const routes: Routes = [
-  {path: '', component: LocationsComponent}
+  {path: '', component: LocationsComponent},
+  {path: 'characters', component: CardsComponent}
 ];
 
 @NgModule({
